ea2e196420980a20521bfec9166ccdeff4646129
A2kmoise <abayomoise950@gmail.com>
2025-04-26 11:11:04 +0200

    shopping 1.0.0

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,3 +24,23 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/boba', (req, res) => res.render('boba'));
 app.use(authRoutes)
+
+//cookies
+
+app.get('/set-cookies', (req, res) => {
+ // res.setHeader('set-Cookie', 'newuser=true')
+
+ res.cookie('newUser', false)
+ res.cookie('isEmployee', true, { maxAge : 1000 * 60 * 60 * 24 , httpOnly : true})
+
+  res.send('you got some cookies')
+
+})
+
+app.get('/read-cookies', (req , res) => {
+
+  const cookies = req.cookies
+  console.log( cookies.newUser )
+
+  res.json(cookies)
+})
